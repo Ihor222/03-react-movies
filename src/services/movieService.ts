83cc34022afca901facdaa8ed836933fb5c731c8
Ihor222 <miskivigor10@gmail.com>
2025-07-16 19:31:38 +0300

    fg

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,22 +1,29 @@
-import {http} from "../libs/api-service.ts";
-import {BEARER_KEY, ROUTES} from "../constants";
-import type {Movie} from "../types/movie.ts";
- interface MoviesResponse {
-    page: number;
-    results: Movie[];
-    total_pages: number;
-    total_results: number;
+import { http } from '../libs/api-service.ts';
+import { BEARER_KEY, ROUTES } from '../constants';
+import type { Movie } from '../types/movie.ts';
+
+interface MoviesResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
 }
-export const fetchMovie = async (query:string,page:string = '1'):Promise<MoviesResponse> => {
-    const urlSearchParams:URLSearchParams = new URLSearchParams({
-        query,
-        page
-    })
-    const {data} = await http.get<MoviesResponse>(`${ROUTES.searchMovie}?${urlSearchParams.toString()}`,{
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MjE5OTZmYmNlNzFlYjI5Y2Q0MDhlMjQ3ODlhNDQyMCIsIm5iZiI6MTc1MjU4NzYwOC43MTksInN1YiI6IjY4NzY1ZDU4ODQ1OWU5NGE4MmE3YjE1MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.e88HARb-tOxlt9j1f4g-Xw9un_SduYbvlgvVFiJ1ypc`
-        }
-    });
-    return data;
-}
+
+export const fetchMovie = async (
+  query: string,
+  page: string = '1'
+): Promise<MoviesResponse> => {
+  const urlSearchParams = new URLSearchParams({ query, page });
+
+  const { data } = await http.get<MoviesResponse>(
+    `${ROUTES.searchMovie}?${urlSearchParams.toString()}`,
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${BEARER_KEY}`, 
+      },
+    }
+  );
+
+  return data;
+};
